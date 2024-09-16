e7e09d4f44a2182e430d0ab66708e8b7d18744af
Mark Berger <maberger0811@gmail.com>
2024-09-16 19:18:28 +0200

    added adyen jobs to the board

--- src/pages/jobboard.js
+++ src/pages/jobboard.js
@@ -35,7 +35,47 @@ const JobBoard = () => {
                     "We've positively impacted over a million working lives, and we're just getting started."],
       url: "https://careers.builders.studio/o/become-a-founder",
     },
-
+    {
+      company: "Adyen",
+      logo: "/images/sponsors/adyen.png",
+      title: "Machine Learning Engineer - MLOps",
+      description: ["Adyen is seeking a Machine Learning Engineer to join our central MLOps team, which is responsible for building platforms and tools for all of our data science teams.",
+                    "In this role, you will play a crucial part in shaping the MLOps ecosystem at Adyen, serving a variety of machine learning and statistical models for both real-time and batch predictions — from optimizing payments to combating fraud."],
+      url: "https://grnh.se/e547fc771us",
+    },
+    {
+      company: "Adyen",
+      logo: "/images/sponsors/adyen.png",
+      title: "Senior Machine Learning Scientist",
+      description: ["Adyen is looking for a Senior Machine Learning Scientist to join our team in Amsterdam, a person sitting at the cornerstone of algorithms, mathematics, and engineering, who can solve problems by designing and implementing production-ready machine learning solutions. You will be responsible for building, developing and deploying algorithms that power data products at Adyen."],
+      url: "https://grnh.se/47a0f8f71us",
+    },
+    {
+      company: "Adyen",
+      logo: "/images/sponsors/adyen.png",
+      title: "Senior Data Engineer",
+      description: ["Adyen is looking for a Senior Data Engineer to join our data solution in Amsterdam. We’re looking for a person that understands the business context and the data needs behind it, and excels in implementing high-quality data pipelines on our Big Data Platform.",
+                    "As a Senior Data Engineer at Adyen, you will play a crucial role in shaping our data infrastructure and ensuring the seamless flow of data."
+      ],
+      url: "https://grnh.se/d430e4fb1us",
+    },
+    {
+      company: "Adyen",
+      logo: "/images/sponsors/adyen.png",
+      title: "Data Platform Engineer",
+      description: ["Sitting at the intersection of Data Engineering, Backend Engineering, and Systems Engineering, Data Platform Engineers at Adyen build the foundational layer of tooling and processes for our on-premise Big Data Platforms.",
+                    "These tools support 10s of products, 100s of developers, and 1000s of daily jobs that add to Adyen’s strong portfolio of capabilities."
+      ],
+      url: "https://grnh.se/e824c0631us",
+    },
+    {
+      company: "Adyen",
+      logo: "/images/sponsors/adyen.png",
+      title: "Senior Machine Learning Scientist, Gen AI",
+      description: ["Our team's mission is to create a Generative AI platform at Adyen that supports various applications based on LLMs. This involves developing platform-oriented components for deploying an LLM backend within Adyen's GPU cluster in Kubernetes, with features like monitoring, access control, rate limiting, prompt debugging, and experiment tracking. We mainly use Open Source frameworks and models."
+      ],
+      url: "https://grnh.se/30ad6cff1us",
+    },
     // Add more job listings as needed
   ];
 
